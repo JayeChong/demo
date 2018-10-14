a5f419ff7774eab8c6305d570b3be9bc66de65eb
zhongyan <zhongyan03@meituan.com>
2018-10-14 22:51:41 +0800

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+    var bargain = document.querySelector("#bargain");
+    var mask = document.querySelector(".mask");
+    var rotateLight = document.querySelector('.rotate_light');
+    var closeBtn = document.querySelector('.close');
+    var btns = document.querySelectorAll('footer .btn');
+
+    bargain.addEventListener('click', toggleMask, true);
+    mask.addEventListener('click', toggleMask, false);
+    rotateLight.addEventListener('click', toggleMask, false);
+    closeBtn.addEventListener('click', toggleMask, false);
+    for(var i=0, len=btns.length; i < len; i++) {
+        // IIFE
+        (function fun(i){
+            btns[i].addEventListener('click', toggleMask, true);
+        })(i);
+    }
+}
+
+function toggleMask(modal) {
+    console.log('test');
+    var modal = document.querySelector(".modal");
+    var showStatus = modal.style.display === "block" ? "none" : "block";
+    modal.style.display = showStatus;
+}
